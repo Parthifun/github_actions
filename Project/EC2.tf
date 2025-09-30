6f6de28f96f6a1805f1f5813abf8e132c0d54126
Parthifun <parthifunreddy29@gmail.com>
2025-09-30 18:15:13 -0400

    Update EC2.tf

--- Project/EC2.tf
+++ Project/EC2.tf
@@ -1,11 +1,10 @@
+provider "aws" {
+  region = "us-east-1"
+}
+
 resource "aws_instance" "TV" {
   ami           = "ami-08982f1c5bf93d976"
   instance_type = "t3.micro"
-  subnet_id = aws_subnet.Kushi.id
-  associate_public_ip_address = true
-  vpc_security_group_ids = [aws_security_group.security.id]
-  key_name = aws_key_pair.my_key.key_name
-
 
   tags = {
     Name = "Channel123"
